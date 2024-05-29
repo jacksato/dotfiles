5e8bfe1a62da8af929183f3742965d1f5fe7eeb3
jacksato <jack.satoshi@gmail.com>
2024-05-28 18:12:55 -0700

    add neorg and fix r-nvim

--- nvim/dot-config/nvim/lua/custom/plugins/r-nvim.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/r-nvim.lua
@@ -1,35 +1,38 @@
 return {
-  "R-nvim/R.nvim",
+  'R-nvim/R.nvim',
   config = function()
     -- Create a table with the options to be passed to setup()
     local opts = {
-      R_args = { "--quiet", "--no-save" },
+      R_args = { '--quiet', '--no-save' },
       hook = {
         on_filetype = function()
           -- This function will be called at the FileType event
           -- of files supported by R.nvim. This is an
           -- opportunity to create mappings local to buffers.
-          vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
-          vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+          vim.api.nvim_buf_set_keymap(0, 'n', '<Enter>', '<Plug>RDSendLine', {})
+          vim.api.nvim_buf_set_keymap(0, 'v', '<Enter>', '<Plug>RSendSelection', {})
         end,
       },
+      insert_mode_cmd = true,
       min_editor_width = 72,
       rconsole_width = 78,
+      pipe_keymap = '',
+      assignment_keymap = '_',
       disable_cmds = {
-        "RClearConsole",
-        "RCustomStart",
-        "RSPlot",
-        "RSaveClose",
+        'RClearConsole',
+        'RCustomStart',
+        'RSPlot',
+        'RSaveClose',
       },
     }
     -- Check if the environment variable "R_AUTO_START" exists.
     -- If using fish shell, you could put in your config.fish:
     -- alias r "R_AUTO_START=true nvim"
-    if vim.env.R_AUTO_START == "true" then
+    if vim.env.R_AUTO_START == 'true' then
       opts.auto_start = 1
       opts.objbr_auto_start = true
     end
-    require("r").setup(opts)
+    require('r').setup(opts)
   end,
   lazy = false,
 }
